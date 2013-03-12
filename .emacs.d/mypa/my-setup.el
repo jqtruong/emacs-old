cba9255c5c5aa8289408da1eceb19fd336b0c74b
Jerome Truong <jtruong@dhcp-23.bravo>
2013-03-12 16:20:37 -0500

    updated shell configs

--- .emacs.d/mypa/my-setup.el
+++ .emacs.d/mypa/my-setup.el
@@ -1,18 +1,11 @@
-(defvar *victaulic-servers*
-  '((web1 . "50.56.17.57")
-    (web2 . "50.56.17.58")
-    (db1  . "50.56.17.59")
-    (db2  . "50.56.17.60")))
-
-(defun victaulic/shell (server-name)
+(defun jqt/shell (server-name project servers)
   ""
-  (interactive "sServer name: ")
   (let (;; Store current directory for later grab. 
         (previous-directory default-directory))
     ;; Change directory to remote server.
-    (cd (format "/nerdery@%s:" (cdr (assoc (intern server-name) *victaulic-servers*))))
+    (cd (format "/%s" (cdr (assoc (intern server-name) servers))))
     ;; Open shell to remote server.
-    (shell (format ";shell victaulic %s" server-name))
+    (shell (format ";shell %s %s" project server-name))
     ;; Switch back to where we currently are.
     (switch-to-buffer nil)
     ;; Change directory back to what it actually is currently.
@@ -20,4 +13,28 @@
     ;; Switch back to shell.
     (switch-to-buffer nil)))
 
+(defvar *victaulic-servers*
+  '((web1 . "nerdery@50.56.17.57:docroot/")
+    (web2 . "nerdery@50.56.17.58:docroot/")
+    (db1  . "nerdery@50.56.17.59:")
+    (db2  . "nerdery@50.56.17.60:")
+    (staging . "jtruong@athens.sierrabravo.net:/vweb/victaulic.sierrabravo.net/docs/")))
+
+(defun victaulic/shell (server-name)
+  ""
+  (interactive "sServer name: ")
+  (jqt/shell server-name "victaulic" *victaulic-servers*))
+
+(defvar *ponderoo-servers*
+  '((django-1 . "root@108.166.7.154:/var/www/python/ponderoo/")
+    (web1 . "")
+    (staging . "jtruong@204.62.150.55:/var/www/qdsolutions.sierrabravo.net/")))
+
+(defun ponderoo/shell (server-name)
+  ""
+  (interactive "sServer name: ")
+  (jqt/shell server-name "ponderoo" *ponderoo-servers*))
+
 (provide 'my-setup)
+
+(cdr (assoc 'django-1 *ponderoo-servers*))
