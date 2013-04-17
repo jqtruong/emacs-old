67b5e2de89f30a6bb5359d2bd3c961eee428e5ad
Jerome Truong <jtruong@Jerome-Truongs-MacBook-Pro.local>
2013-04-17 12:13:10 -0500

    updated emacs

---
+++ .emacs.d/elpa/jira-20091012.2123/jira-autoloads.el
@@ -0,0 +1,18 @@
+;;; jira-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("jira-pkg.el" "jira.el") (20836 6342 188258
+;;;;;;  0))
+
+;;;***
+
+(provide 'jira-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jira-autoloads.el ends here
---
+++ .emacs.d/elpa/org-jira-20130319.1601/org-jira-pkg.el
@@ -0,0 +1,2 @@
+(define-package "org-jira" "20130319.1601" "Syncing between Jira and Org-mode. [source: github]" 'nil)
+
